07fb9470f33d679d63399cef7c3c0c6d07ac8ba0
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2023-12-09 12:00:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+function rgbToHex(rgb) {
+    // 将RGB格式的颜色值拆分为红色、绿色和蓝色的相应部分。
+    const [r, g, b] = rgb.match(/\d+/g);
+    
+    // 将每个部分的整数值转换为两位的十六进制值。
+    const rHex = parseInt(r).toString(16).padStart(2, '0');
+    const gHex = parseInt(g).toString(16).padStart(2, '0');
+    const bHex = parseInt(b).toString(16).padStart(2, '0');
+    
+    // 将三个部分的十六进制值合并为一个HEX格式的颜色值。
+    const hexColor = `#${rHex}${gHex}${bHex}`;
+    
+    return hexColor;
+  }
+
+function checkInput() {
+    var input = document.getElementById("input-text-right");
+    var button = document.getElementById("g_button");
+    
+    if (input.value === "") {
+        button.classList.add("disabled");
+        button.innerHTML = "Required right text";
+    } else {
+        button.classList.remove("disabled");
+        button.innerHTML = "Generate Badge";
+    }
+}
+
 function generateBadge() {
     var leftText = document.getElementById('input-text').value || '';
     var rightText = document.getElementById('input-text-right').value || '';
-    var textColor = document.getElementById('input-color').value || 'default';
+    var textColor = rgbToHex(getComputedStyle(document.getElementById('input-color')).getPropertyValue("background-color")) || '';
     var url = document.getElementById('input-url').value || '';
     var logo = document.getElementById('input-logo').value || '';
-    var logoColor = document.getElementById('input-logo-color').value || '';
+    var logoColor = rgbToHex(getComputedStyle(document.getElementById('input-logo-color')).getPropertyValue("background-color")) || '';
 
     // 如果rightText为空，则不执行任何操作    
     if (rightText === '') {
@@ -22,7 +50,7 @@ function generateBadge() {
         if (logoColor === '') {
             badgeUrl = badgeUrl + '?logo=' + logo;
         } else {
-            badgeUrl = badgeUrl + '?logo=' + logo + '&logoColor=' + logoColor;
+            badgeUrl = badgeUrl + '?logo=' + logo + '&logoColor=' + logoColor.replace(/[^a-zA-Z0-9]/g, '');
         }
     }
 
@@ -43,3 +71,5 @@ function generateBadge() {
         document.getElementById('result-md').innerHTML = "[" + md + "](" + url + ")";
     }
 }
+
+checkInput();
