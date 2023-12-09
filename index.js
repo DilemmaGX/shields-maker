0ff13f5d91ce5d157801862d8ab49ecf19580cd8
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2023-12-09 13:51:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 function rgbToHex(rgb) {
     // 将RGB格式的颜色值拆分为红色、绿色和蓝色的相应部分。
     const [r, g, b] = rgb.match(/\d+/g);
-    
+
     // 将每个部分的整数值转换为两位的十六进制值。
     const rHex = parseInt(r).toString(16).padStart(2, '0');
     const gHex = parseInt(g).toString(16).padStart(2, '0');
     const bHex = parseInt(b).toString(16).padStart(2, '0');
-    
+
     // 将三个部分的十六进制值合并为一个HEX格式的颜色值。
     const hexColor = `#${rHex}${gHex}${bHex}`;
-    
+
     return hexColor;
-  }
+}
+
+function escapeHtml(html) {
+    return html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
 
 function checkInput() {
     var input = document.getElementById("input-text-right");
     var button = document.getElementById("g_button");
-    
+
     if (input.value === "") {
         button.classList.add("disabled");
         button.innerHTML = "Required right text";
@@ -33,6 +37,7 @@ function generateBadge() {
     var url = document.getElementById('input-url').value || '';
     var logo = document.getElementById('input-logo').value || '';
     var logoColor = rgbToHex(getComputedStyle(document.getElementById('input-logo-color')).getPropertyValue("background-color")) || '';
+    var style = document.getElementById('styleSelect').value;
 
     // 如果rightText为空，则不执行任何操作    
     if (rightText === '') {
@@ -48,10 +53,12 @@ function generateBadge() {
 
     if (logo != '') {
         if (logoColor === '') {
-            badgeUrl = badgeUrl + '?logo=' + logo;
+            badgeUrl = badgeUrl + '?style=' + style + '&logo=' + logo;
         } else {
-            badgeUrl = badgeUrl + '?logo=' + logo + '&logoColor=' + logoColor.replace(/[^a-zA-Z0-9]/g, '');
+            badgeUrl = badgeUrl + '?style=' + style + '&logo=' + logo + '&logoColor=' + logoColor.replace(/[^a-zA-Z0-9]/g, '');
         }
+    } else {
+        badgeUrl = badgeUrl + '?style=' + style;
     }
 
     var html = '<a href="' + url + '"><img src="' + badgeUrl + '" alt="' + leftText + ' - ' + rightText + '"></a>';
@@ -59,16 +66,18 @@ function generateBadge() {
 
     document.getElementById('result-html').innerHTML = html;
     if (url === '') {
-        var html = '<a href="' + badgeUrl + '"><img src="' + badgeUrl + '" alt="' + leftText + ' - ' + rightText + '"></a>';
+        html = '<a href="' + badgeUrl + '"><img src="' + badgeUrl + '" alt="' + leftText + ' - ' + rightText + '"></a>';
         document.getElementById('result-html').innerHTML = html;
+        document.getElementById('result-pure').innerText = html;
         document.getElementById('result-md').innerHTML = md;
     } else {
         if (!/^https?:\/\//.test(url)) {
             url = "http://" + url;
         }
-        var html = '<a href="' + url + '"><img src="' + badgeUrl + '" alt="' + leftText + ' - ' + rightText + '"></a>';
+        html = '<a href="' + url + '"><img src="' + badgeUrl + '" alt="' + leftText + ' - ' + rightText + '"></a>';
         document.getElementById('result-html').innerHTML = html;
-        document.getElementById('result-md').innerHTML = "[" + md + "](" + url + ")";
+        document.getElementById('result-pure').innerText = html;
+        document.getElementById('result-md').innerText = "[" + md + "](" + url + ")";
     }
 }
 
